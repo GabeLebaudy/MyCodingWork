8740c1c5439d1ecfb35c4f27bde315e77ce3fc49
GabeLebaudy <99764213+GabeLebaudy@users.noreply.github.com>
2023-01-15 11:05:49 -0500

    Adding JavaScript Section

--- Learning JS/JSBasics.js
+++ Learning JS/JSBasics.js
@@ -1,2 +1,5 @@
 //Console.log() is the same as System.out.println() or print() etc.
-console.log("This is my JS Tutorial!")
+console.log("This is my JS Tutorial!");
+//Use the Alert function to have a message box pop up: This might only work as a script on a webpage
+alert("This is an example of what the alert function does!"); 
+
