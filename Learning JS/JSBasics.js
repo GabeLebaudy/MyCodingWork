59ec9de07b7331392025c6fa1ad85bbbe755cb10
GabeLebaudy <99764213+GabeLebaudy@users.noreply.github.com>
2023-01-20 13:30:31 -0500

    Updating Repo

--- Learning JS/JSBasics.js
+++ Learning JS/JSBasics.js
@@ -1,6 +1,13 @@
 //Console.log() is the same as System.out.println() or print() etc.
 console.log("This is my JS Tutorial!");
 var exampleVariable = 15;
+//arithmetic, increment operators
+exampleVariable++;
+exampleVariable--;
+exampleVariable *= 5;
+exampleVariable /= 15
+exampleVariable += 15
+exampleVariable -= 4;
 console.log("The Variable's Value Is: " + exampleVariable);
 
 //Use the Alert function to have a message box pop up: This might only work as a script on a webpage
@@ -11,6 +18,7 @@ document.getElementById("LA_V_Jacksonville").innerText = "Jacksonville V.S. Los 
 document.getElementById("Miami_V_Buffalo").innerText = "Miami V.S. Buffalo: Honestly did not expect this game to go the way it did, I thought the dolphins would not be able to get anything going, which was mostly true, however Josh Allen was nearly responsible for all points scored in the game.";
 document.getElementById("NewYork_V_Minnesota").innerText = "New York V.S. Minnesota: I thought the score would be lower, but I was right on the money for this game, Giants win and cover the spread.";
 document.getElementById("Baltimore_V_Cincinatti").innerText = "Baltimore V.S. Cincinnati: Game was much closer than I expected with Tyler Huntly at the QB spot. Ravens could be dangerous next year if Lamar chooses to stay.";
+document.getElementById("TampaBay_V_Dallas").innerText = "Tampa Bay V.S. Dallas: Honestly most unpredictable game and I was wrong. Didn't really expect this outcome."
 document.getElementById("sampleHeader").innerText = exampleVariable + " is the value of this variable!";
 //The examples above use the .innerText command, however, .innerHTML will modify the HTML Structure itself.
 myElement = document.getElementById("testingParagraph");
