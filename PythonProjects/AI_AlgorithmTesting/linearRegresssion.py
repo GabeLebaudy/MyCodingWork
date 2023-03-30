a0754a0b999ebe059afed129a07d9b448612f93b
GabeLebaudy <99764213+GabeLebaudy@users.noreply.github.com>
2023-03-30 17:31:58 -0400

    Documented Linear Regression

--- PythonProjects/AI_AlgorithmTesting/linearRegresssion.py
+++ PythonProjects/AI_AlgorithmTesting/linearRegresssion.py
@@ -9,26 +9,41 @@ import os
 
 #Loss function: Not needed just for math reference
 def loss_function(m, b, dataPoints):
+    #Initialize error var
     totalError = 0
+    
+    #Run for amount of dataPoints
     for i in range(0, len(dataPoints)):
+        #Set x and y to values of current data point
         xVal = dataPoints.iloc[i].x
         yVal = dataPoints.iloc[i].y
+        
+        #Error is equal to actual yVal, subtracted by expected value squared to make all values positive
         totalError += (yVal - (m * xVal + b)) ** 2
+    
+    #Mean error is equal to total error divided by points    
     totalError /= float(len(dataPoints))
     return totalError
 
 
 #Derivative of function above
 def gradient_descent(m_now, b_now, dataPoints, L):
+    #Set dE/dm and dE/db to 0, and n to number of points in dataSet
     m_gradient = 0
     b_gradient = 0
     n = len(dataPoints)
+    
+    #Run for how many points exist in database
     for i in range(n):
+        #Set x and y to their values in each entry of the CSV file
         x = dataPoints.iloc[i].x
         y = dataPoints.iloc[i].y
-        m_gradient += -(2/n) * x * (y - (m_now * x + b_now))
-        b_gradient += -(2/n) * (y - (m_now * x + b_now))
         
+        #Set dE/dm and dE/db to values based on current m and b, plus current x and y values
+        m_gradient += -(2/n) * x * (y - (m_now * x + b_now)) 
+        b_gradient += -(2/n) * (y - (m_now * x + b_now))
+    
+    #Move m and b in opposite direction of greatest ascent.   
     m = m_now - m_gradient * L
     b = b_now - b_gradient * L
     return m, b
@@ -45,17 +60,19 @@ if __name__ == "__main__":
     plt.show()
     '''
     #Run function many times and have the AI 'learn' over time
-    m = 0
-    b = 0
-    L = 0.0001
+    m = 0 #Slope Value
+    b = 0 #Y-intercept Value
+    L = 0.0001 #Learning Rate: Larger numbers are faster but less accurate
     Iterations = 1000
-    for i in range(Iterations):
+    for i in range(Iterations): #AI to run function and improve m and b values each iteration
         if i % 50 == 0:
             print(f"Iteration: {i}")
         m , b = gradient_descent(m, b, randomData, L)
     
+    #Print final m and b values
     print(m, b)
     
+    #Visualize data with matplotlib
     plt.scatter(randomData.x, randomData.y, color="red")
     plt.plot(list(range(0, 15)), [m * x + b for x in range(0, 15)], color="black")
     plt.show()
