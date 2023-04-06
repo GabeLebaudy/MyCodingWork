d191f1585f599aa956ac86ba4bd857de5aee2c58
GabeLebaudy <99764213+GabeLebaudy@users.noreply.github.com>
2023-04-06 13:24:22 -0400

    Keeping Repo Up to Date

---
+++ PythonProjects/NumPyPractice/NumPyPractice.py
@@ -0,0 +1,10 @@
+#Testing out the linspace function in numpy for x-ticks in the sensor data plotting
+
+import numpy as np
+
+
+if __name__ == "__main__":
+    xArray = np.linspace(1, 10, 19)
+    print(xArray)
+    
+    
