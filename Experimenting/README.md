4b17dd30ad41869bda351442a18e6e91f1678b1e
gl523 <gabe.lebaudy6@gmail.com>
2023-10-09 18:38:20 -0400

    Python Folder Moves

---
+++ Experimenting/README.md
@@ -0,0 +1,7 @@
+# Experimenting Folder
+
+This folder is just for learning new languages and testing out code snippets. 
+
+Nothing in here is a complete project.
+
+Experimental areas are divided by language.
---
+++ PythonProjects/YT Video Downloading/downloadFile.py
@@ -0,0 +1,9 @@
+#The purpose of this file will be to download youtube videos, when giving a video URL.
+
+#Import Modules
+from pytube import YouTube
+
+#GUI Modules
+from PyQt6.QtWidgets import (
+    QMainWindow, QApplication
+)
