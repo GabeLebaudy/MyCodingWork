38866d726ce3041477e0b05bb09c849e07e09a7a
gl523 <gabe.lebaudy6@gmail.com>
2024-01-22 20:46:41 -0500

    Quiz Updates

--- PythonProjects/Quizlet Type Project/quiz.py
+++ PythonProjects/Quizlet Type Project/quiz.py
@@ -15,9 +15,9 @@ import math
 
 class QuizQuestion:
     #Constructor
-    def __init__(self, q, a):
-        self.question = q
-        self.answer = a
+    def __init__(self):
+        self.question = None
+        self.answer = None
 
     #Getters
     def getQuestion(self):
@@ -40,32 +40,208 @@ class QuizQuestion:
             temp = self.question
             self.question = self.answer
             self.answer = temp
+        return doSwitch
 
 #Class for storing data about a multiple choice question
 class MultipleChoiceQuestion(QuizQuestion):
     #Constructor
-    def __init__(self, q, a):
-        super().__init__(q, a)
+    def __init__(self, main_layout, q_layout, q_label, btn_container, btn_space, btn_layout, btn_list):
+        super().__init__()
+        self.main_layout = main_layout
+        self.question_layout = q_layout
+        self.question_label = q_label
+        self.button_container = btn_container
+        self.button_space_layout = btn_space
+        self.button_layout = btn_layout
+        self.button_list = btn_list
+
+        self.correct_answer_index = 0
+    
+    #Set the question and answer properties of the parent object, then set the question label accordingly
+    def setQuestionAndAnswer(self, q, a, gamemode):
+        #Set question and answer in parent object
+        super().setQuestion(q)
+        super().setAnswer(a)
+
+        #Check if the user wants to randomize the question
+        if gamemode == 0:
+            didSwitch = super().randomize() #Initially definition will always be question, if it did switch, the term is the question
+            if didSwitch == 0: #The term is the question, and definition is the answer
+                self.question_label.setText("What is the definition of {}?".format(super().getQuestion()))
+            else: #Did not switch, definition is the question
+                self.question_label.setText("What term is defined by {}?".format(super().getQuestion()))
+
+        if gamemode == 1: #Definition is question
+            self.question_label.setText("What is the definition of {}?".format(q))
+
+        if gamemode == 2: #Term is question
+            self.question_label.setText("What term is defined by {}?".format(q))
+
+    #Get current answer
+    def getAnswer(self):
+        return super().getAnswer()
+
+    #Set the answers
+    def setAnswerOptions(self, all_answers):
+        #Store the index of the radio button with the correct answer
+        self.correct_answer_index = random.randint(0, 3)
+
+        #Find 3 answers that are not the same, and are not the correct answer
+        answer_sub_ind = all_answers.index(super().getAnswer())
+
+        incorrect_answers = []
+        while not incorrect_answers or answer_sub_ind in incorrect_answers:
+            incorrect_answers = random.sample(range(len(all_answers)), 3)
+
+        #Set the text of each radio button
+        for i in range(4):
+            if i == self.correct_answer_index: #Correct radio button answer
+                self.button_list[i].setText(super().getAnswer())
+            else:
+                ind = incorrect_answers.pop(0)
+                self.button_list[i].setText(all_answers[ind])
+
+        
+
+    #Delete the question (On restart and game end)
+    def deleteQuestion(self):
+        #Start from bottom of the layouts, and remove upward
+        for btn in self.button_list:
+            btn.deleteLater()
+
+        self.button_layout.deleteLater()
+        self.button_space_layout.deleteLater()
+        self.button_container.deleteLater()
+        self.question_label.deleteLater()
+        self.question_layout.deleteLater()
+        self.main_layout.deleteLater()
 
 #Class for storing data about a matching question
 class MatchingQuestion(QuizQuestion):
     #Constructor
-    def __init__(self, q, a):
-        super().__init__(q, a)
+    def __init__(self, main_layout, input_field, input_label, answer_label):
+        super().__init__()
+        self.main_layout = main_layout
+        self.input_field = input_field
+        self.input_label = input_label
+        self.answer_label = answer_label
+
+    #Set question and answer in parent layout and question label
+    def setQuestionAndAnswer(self, q, a, gamemode):
+        #Set question and answer in parent object
+        super().setQuestion(q)
+        super().setAnswer(a)
+
+        #Depending on gamemode, set questions accordingly
+        if gamemode == 0:
+            didSwitch = super().randomize() #Initially definition will always be question, if it did switch, the term is the question
+            if didSwitch == 0: #The term is the question, and definition is the answer
+                self.input_label.setText(super().getQuestion())
+            else: #Did not switch, definition is the question
+                self.input_label.setText(super().getQuestion())
+
+        if gamemode == 1: #Definition is question
+            self.input_label.setText(q)
+
+        if gamemode == 2: #Term is question
+            self.input_label.setText(q)
+
+    #Get current answer
+    def getAnswer(self):
+        return super().getAnswer()
+    
+    #Set the corresponding answer, and store index for checking the answer
+    def setAnswerLabel(self, loop_index):
+        pass
+
+    #Delete the question
+    def deleteQuestion(self):
+        self.answer_label.deleteLater()
+        self.input_label.deleteLater()
+        self.input_field.deleteLater()
+        self.main_layout.deleteLater()
 
 #Class for storing data about a true or false question
 class TrueFalseQuestion(QuizQuestion):
     #Constructor
-    def __init__(self, q, a):
-        super().__init__(q, a)
+    def __init__(self, main_layout, question_layout, question_label, answer_container, answer_space_layout, answer_layout, true_button, false_button):
+        super().__init__()
+        self.main_layout = main_layout
+        self.question_layout = question_layout
+        self.question_label = question_label
+        self.answer_container = answer_container
+        self.answer_space_layout = answer_space_layout
+        self.answer_layout = answer_layout
+        self.true_button = true_button
+        self.false_button = false_button
 
+        self.true_false_answer = True
+    
+    #Set question and answer of parent container
+    def setQuestionAndAnswer(self, q, a, gamemode):
+        #Set properties of parent layout
+        super().setQuestion(q)
+        super().setAnswer(a)
+
+        #If user selected mixed, randomize question and answer
+        if gamemode == 0:
+            super().randomize()
+
+    #Get current answer
+    def getAnswer(self):
+        return super().getAnswer()
+    
+    #Set question label
+    def setQuestionLabel(self, all_answers, gamemode):
+        #Randomly select if the answer is true or false
+        self.true_false_answer = random.choice([True, False])
+
+        #Set the text of the label based on the gamemode
+        if gamemode == 0:
+            if self.true_false_answer: #True
+                pass
+
+        if gamemode == 1: #Definition is the question
+            pass
+
+        if gamemode == 2: #Term is the question
+            pass
 #Class for storing data about a type answer question
 class TypeAnswerQuestion(QuizQuestion):
     #Constructor
-    def __init__(self, q, a):
-        super().__init__(q, a)
+    def __init__(self, main_layout, label_layout, question_label, input_layout, answer_input):
+        super().__init__()
+        self.main_layout = main_layout
+        self.label_layout = label_layout
+        self.question_label = question_label
+        self.input_layout = input_layout
+        self.answer_input = answer_input
 
-#Class for storing data about
+    #Set question and answer properties of parent object and set the question label
+    def setQuestionAndAnswer(self, q, a, gamemode):
+        #Set parent properties
+        super().setQuestion(q)
+        super().setAnswer(a)
+
+        #Use user selected gamemode to determine question label's text
+        #Check if the user wants to randomize the question
+        if gamemode == 0:
+            didSwitch = super().randomize() #Initially definition will always be question, if it did switch, the term is the question
+            if didSwitch == 0: #The term is the question, and definition is the answer
+                self.question_label.setText("What is the definition of {}?".format(super().getQuestion()))
+            else: #Did not switch, definition is the question
+                self.question_label.setText("What term is defined by {}?".format(super().getQuestion()))
+
+        if gamemode == 1: #Definition is question
+            self.question_label.setText("What is the definition of {}?".format(q))
+
+        if gamemode == 2: #Term is question
+            self.question_label.setText("What term is defined by {}?".format(q))
+
+    #Get current answer
+    def getAnswer(self):
+        return super().getAnswer()
+
 
 #Quiz Class
 class Quiz(QObject):
@@ -82,6 +258,7 @@ class Quiz(QObject):
 
         self.question_types = []
         self.gamemode = 0
+        self.all_answers = []
 
         #For getting set data
         self.set_data = Sets()
@@ -356,6 +533,7 @@ class Quiz(QObject):
     
     #Add a multiple choice question
     def addMultipleChoiceQuestion(self):
+        #Generate the layout and widgets
         main_mc_layout = QVBoxLayout()
 
         question_layout = QHBoxLayout()
@@ -389,6 +567,10 @@ class Quiz(QObject):
         
         self.multiple_choice_questions_layout.addLayout(main_mc_layout)
         self.multiple_choice_questions_layout.addSpacing(int(25 * self.heightScale))
+        
+        #Create Object for storage
+        mc_question = MultipleChoiceQuestion(main_mc_layout, question_layout, question_label, radio_button_container, radio_button_space_layout, radio_button_layout, radio_buttons)
+        self.multiple_choice_questions.append(mc_question)
 
     #Add a matching question
     def addMatchingQuestion(self):
@@ -417,6 +599,10 @@ class Quiz(QObject):
         
         self.matching_questions_layout.addLayout(main_matching_layout)
         self.matching_questions_layout.addSpacing(int(25 * self.heightScale))
+
+        #Create and store object
+        matching_obj = MatchingQuestion(main_matching_layout, matching_input, matching_input_label, matching_answer_label)
+        self.matching_questions.append(matching_obj)
 
     #Add a true or false question
     def addTrueFalseQuestion(self):
@@ -457,6 +643,10 @@ class Quiz(QObject):
         self.true_false_questions_layout.addLayout(main_true_false_layout)
         self.true_false_questions_layout.addSpacing(int(25 * self.heightScale))
 
+        #Create and store object
+        tf_object = TrueFalseQuestion(main_true_false_layout, true_false_question_layout, true_false_question_label, true_false_options_container, true_false_space_layout, true_false_options_layout, true_button, false_button)
+        self.true_false_questions.append(tf_object)
+
     #Add a type answer question
     def addTypeAnswerQuestion(self):
         main_type_answer_layout = QVBoxLayout()
@@ -488,6 +678,10 @@ class Quiz(QObject):
         self.type_answer_questions_layout.addLayout(main_type_answer_layout)
         self.type_answer_questions_layout.addSpacing(int(25 * self.heightScale))
 
+        #Create and store object
+        type_obj = TypeAnswerQuestion(main_type_answer_layout, main_type_answer_label_layout, main_type_answer_label, main_type_answer_input_layout, main_type_answer_input)
+        self.type_answer_questions.append(type_obj)
+
     #Show/Hide the main container
     def setHidden(self, status):
         self.main_quiz_container.setHidden(status)
@@ -518,39 +712,60 @@ class Quiz(QObject):
         num_per_question_type = math.floor(len(set_content) / divisor)
         num_extra_questions = len(set_content) % divisor
 
+        #Generate and populate all questions
+        self.genAllQuestions(num_per_question_type, num_extra_questions)
+
+        self.fillAllQuestions(set_content)
+
+        for mc in self.multiple_choice_questions:
+            self.all_answers.append(mc.getAnswer())
+
+        for ma in self.matching_questions:
+            self.all_answers.append(ma.getAnswer())
+
+        for tf in self.true_false_questions:
+            self.all_answers.append(tf.getAnswer())
+
+        for ta in self.type_answer_questions:
+            self.all_answers.append(ta.getAnswer())
+
+        self.fillAllAnswers()
+
+    #Function for generating questions
+    def genAllQuestions(self, baseline, extra):
         num_mc, num_ma, num_tf, num_ta = 0,0,0,0
 
         #Check multiple choice first since I chose a stupid order
         if self.question_types[0]:
             self.mc_sub_title_container.setHidden(False)
-            num_mc = num_per_question_type
-            if (self.question_types[1] and num_extra_questions > 1) or (not(self.question_types[1]) and num_extra_questions > 0): #Add extra question to mc if conditions are right
+            num_mc = baseline
+            if (self.question_types[1] and extra > 1) or (not(self.question_types[1]) and extra > 0): #Add extra question to mc if conditions are right
                 num_mc += 1
-                num_extra_questions -= 1
+                extra -= 1
         else:
             self.mc_sub_title_container.setHidden(True)
             
         if self.question_types[1]:
             self.ma_sub_title_container.setHidden(False)
-            num_ma = num_per_question_type
-            if num_extra_questions > 0:
+            num_ma = baseline
+            if extra > 0:
                 num_ma += 1
-                num_extra_questions -= 1
+                extra -= 1
         else:
             self.ma_sub_title_container.setHidden(True)
 
         if self.question_types[2]:
             self.tf_sub_title_container.setHidden(False)
-            num_tf = num_per_question_type
-            if num_extra_questions:
+            num_tf = baseline
+            if extra:
                 num_tf += 1
-                num_extra_questions -= 1
+                extra -= 1
         else:
             self.tf_sub_title_container.setHidden(True)
 
         if self.question_types[3]:
             self.ta_sub_title_container.setHidden(False)
-            num_ta = num_per_question_type
+            num_ta = baseline
         else:
             self.ta_sub_title_container.setHidden(True)
 
@@ -567,7 +782,61 @@ class Quiz(QObject):
         for ta in range(num_ta):
             self.addTypeAnswerQuestion()
 
+    #Fill all question labels
+    def fillAllQuestions(self, set_data):
+        #Fill multiple choice question labels
+        for mc in range(len(self.multiple_choice_questions)):
+            #Pull then delete data pair
+            contents = set_data[0].split(':')
+            set_data.pop(0)
+            if self.gamemode < 2: #Definition is the question, at least initially 
+                self.multiple_choice_questions[mc].setQuestionAndAnswer(contents[1], contents[0], self.gamemode)
+            else: #Definition is the term
+                self.multiple_choice_questions[mc].setQuestionAndAnswer(contents[0], contents[1], self.gamemode)
 
+        #Fill matching question labels
+        for ma in range(len(self.matching_questions)):
+            #Pull then delete data pair
+            contents = set_data[0].split(':')
+            set_data.pop(0)
+            if self.gamemode < 2: #Definition is the question, at least initially 
+                self.matching_questions[ma].setQuestionAndAnswer(contents[1], contents[0], self.gamemode)
+            else: #Definition is the term
+                self.matching_questions[ma].setQuestionAndAnswer(contents[0], contents[1], self.gamemode)
+
+        #Set True/False Questions and Answers(Label is set during answer functions)
+        for tf in range(len(self.true_false_questions)):
+            #Pull then delete data pair
+            contents = set_data[0].split(':')
+            set_data.pop(0)
+            if self.gamemode < 2: #Definition is the question, at least initially 
+                self.true_false_questions[tf].setQuestionAndAnswer(contents[1], contents[0], self.gamemode)
+            else: #Definition is the term
+                self.true_false_questions[tf].setQuestionAndAnswer(contents[0], contents[1], self.gamemode)
+        
+        #Fill out type answer labels        
+        for ta in range(len(self.type_answer_questions)):
+            #Pull then delete data pair
+            contents = set_data[0].split(':')
+            set_data.pop(0)
+            if self.gamemode < 2: #Definition is the question, at least initially 
+                self.type_answer_questions[ta].setQuestionAndAnswer(contents[1], contents[0], self.gamemode)
+            else: #Definition is the term
+                self.type_answer_questions[ta].setQuestionAndAnswer(contents[0], contents[1], self.gamemode)
+            
+    #Fill all answers of each question type
+    def fillAllAnswers(self):
+        #Multiple Choice Questions
+        for mc in self.multiple_choice_questions:
+            mc.setAnswerOptions(self.all_answers)
+
+        #Matching
+        for ma in self.matching_questions:
+            pass
+
+        #True False
+        for tf in self.true_false_questions:
+            tf.setQuestionLabel(self.all_answers, self.gamemode)
 
     #Populate Set Dropdown
     def populateSetDD(self):
@@ -604,6 +873,7 @@ class Quiz(QObject):
         self.type_answer_questions = []
     
         self.gamemode = 0
+        self.all_answers = []
 
     #Clear all questions from the scroll area
     def clearQuestions(self):
