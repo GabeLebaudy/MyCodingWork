29d43263362bb6c53376539bdbcd2212b707d5c8
gl523 <gabe.lebaudy6@gmail.com>
2024-03-04 16:06:25 -0500

    Small NN Changes

--- PythonProjects/MNIST_NN/scratch_NN.py
+++ PythonProjects/MNIST_NN/scratch_NN.py
@@ -278,7 +278,7 @@ if __name__ == "__main__":
         layers.append(10)
         
         print('Test {}. Alpha:{}, Beta:{}, Training Iterations:{}, Layers:{}'.format(i + 1, alpha, beta, train_iterations, layers))
-        #final_parameters = train_model(layer_dims = [175, 64, 10], learning_rate = 0.005, beta = 0.9, num_iterations = 500_000, doTrain = True)
+        final_parameters = train_model(layer_dims = [175, 64, 10], learning_rate = 0.005, beta = 0.9, num_iterations = 150_000, doTrain = True)
         accuracy = test_model()
         print('Test {} accuracy: {}'.format(i + 1, accuracy))
         
