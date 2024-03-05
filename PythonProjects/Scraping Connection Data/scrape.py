883c0ef3dbe2d04f38232e132ffbf6c16ea81ed2
gl523 <gabe.lebaudy6@gmail.com>
2024-03-05 11:26:59 -0500

    Orginization Updates

--- PythonProjects/Scraping Connection Data/scrape.py
+++ PythonProjects/Scraping Connection Data/scrape.py
@@ -9,8 +9,8 @@ from selenium.webdriver.common.action_chains import ActionChains
 
 import time
 
-#Method for pulling data given the URL
-def getInternetData(url):
+#Get the website driver object, and the object for controlling the mouse positioning
+def prepWebsite(url):
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
 
@@ -20,69 +20,71 @@ def getInternetData(url):
     #Create Action Chains object for moving the mouse to specific spots on the screen
     mouse_controller = ActionChains(driver)
     
+    return driver, mouse_controller
+
+#Method for pulling data given the URL
+def getInternetData(url):
+    driver, mouse = prepWebsite(url)
+
     time.sleep(1)
     
+    #Get the distance between rectangle elements that when hovered over display the information about HD streams
     all_rects = driver.find_elements(By.TAG_NAME, 'rect')
-    mouse_cords = []
-    for i in range(len(all_rects)):
-        if all_rects[i].get_attribute('fill') != "#cccccc":
-            continue
-       
-        if all_rects[i].get_attribute('x'):
-            current_cord = int(all_rects[i].get_attribute('x'))
-            
-        if len(mouse_cords) < 1:
-            mouse_cords.append(current_cord)
-            continue
-        
-        if mouse_cords[len(mouse_cords) - 2] > current_cord:
-            break
-        
-        mouse_cords.append(current_cord)
+    mouse_cords = processRects(all_rects)
     
     mouse_cords[1:] = findRightMovements(mouse_cords)
     mouse_cords.pop(-1)
     
     time.sleep(1)
         
-    #Element to move mouse to hover over
-    graph_element = driver.find_element(By.TAG_NAME, 'svg')
+    #Pull elements for getting streaming data
+    graph_element = driver.find_element(By.TAG_NAME, 'svg') #Element to move mouse to hover over
+    data_storage_element = driver.find_element(By.XPATH, "//div[@id ='chart']/div[1]/div[3]") #Div that contains the tooltip which contains the HD% Value
+
+    #Find the compare providers button, and click on it
+    compare_providers_button = driver.find_element(By.CLASS_NAME, 'tab-label')
+    mouse.move_to_element(compare_providers_button)
+    mouse.perform()
+    mouse.click()
     
-    #Find the width of the svg element
-    distance_to_move = (int(graph_element.get_attribute('width')) // 2) - 5
-    
-    #Div that contains the tooltip which contains the HD% Value
-    div_element = driver.find_element(By.XPATH, "//div[@id ='chart']/div[1]/div[3]")
-    
-    mouse_controller.move_to_element_with_offset(graph_element, distance_to_move * -1, 0)
-    mouse_controller.perform()
-    
-    time.sleep(1)
-    
-    gotData = True
-    upOrDown = 1
-    #Get the data
-    for movement in mouse_cords:
-        mouse_controller.move_by_offset(movement, 0)
-        mouse_controller.perform()
-        time.sleep(1)
-        gotData = processToolTipText(div_element.text)
-        num_trys = 1
-        while not gotData:
-            if num_trys >= 3:
-                break
-            
-            mouse_controller.move_by_offset(0, 10 * upOrDown)
-            mouse_controller.perform()
-            upOrDown *= -1 
-            time.sleep(0.1)
-            gotData = processToolTipText(div_element.text)
-            num_trys += 1
+    #Find all High Definition Buttons
+    getHighDefinitionStreams(mouse, graph_element, data_storage_element, mouse_cords)
     
     time.sleep(1)
     
     driver.quit()
 
+
+#Get the % of streams that are HD for each hour
+def getHighDefinitionStreams(mouse, graph, data_div, mouse_cords):
+    #Find the width of the svg element
+    distance_to_move = (int(graph.get_attribute('width')) // 2) - 5
+
+    #Move the mouse to the left side of the graph element
+    mouse.move_to_element_with_offset(graph, distance_to_move * -1, 0)
+    mouse.perform()
+    
+    time.sleep(1)
+    
+    #Get the data
+    gotData = True
+    upOrDown = 1
+    for movement in mouse_cords:
+        mouse.move_by_offset(movement, 0)
+        mouse.perform()
+        time.sleep(1)
+        gotData = processToolTipText(data_div.text)
+        num_trys = 1
+        while not gotData:
+            if num_trys >= 3:
+                break
+            
+            mouse.move_by_offset(0, 10 * upOrDown)
+            mouse.perform()
+            upOrDown *= -1 
+            time.sleep(0.1)
+            gotData = processToolTipText(data_div.text)
+            num_trys += 1
 
 #Process Individual Time Text
 def processToolTipText(text):
@@ -98,8 +100,29 @@ def processToolTipText(text):
     print(hd_percentage, time_frame) 
     
     return True
-    
-#Find Right Movements
+
+#Process which rectangles are the correct ones for mining the data
+def processRects(rects):
+    mouse_cords = []
+    for i in range(len(rects)):
+        if rects[i].get_attribute('fill') != "#cccccc":
+            continue
+       
+        if rects[i].get_attribute('x'):
+            current_cord = int(rects[i].get_attribute('x'))
+            
+        if len(mouse_cords) < 1:
+            mouse_cords.append(current_cord)
+            continue
+        
+        if mouse_cords[len(mouse_cords) - 2] > current_cord:
+            break
+        
+        mouse_cords.append(current_cord)
+
+    return mouse_cords
+
+#Find Distance to move the mouse to the right each time for video definition data
 def findRightMovements(coords):
     temp = []
     for i in range(1, len(coords)):
