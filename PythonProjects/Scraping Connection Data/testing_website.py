27918e043c364f4d43e963797dbb3deb41ef84a1
GabeLebaudy <99764213+GabeLebaudy@users.noreply.github.com>
2024-03-20 15:07:29 -0400

    Test

--- PythonProjects/Scraping Connection Data/testing_website.py
+++ PythonProjects/Scraping Connection Data/testing_website.py
@@ -13,6 +13,7 @@ import time
 import sys
 import os
 import re
+import subprocess
 
 #Helper Methods
 def prepWebsite(url):
@@ -151,93 +152,23 @@ def testCropImage():
 def testRegularExpression(text):
     return re.search("[0-9+][%+]", text)
 
-
-#Test all states, ensure that all of them are able to be looked up in the website
-def testAllStates(state):
-    driver, mouse = prepWebsite("https://www.google.com/get/videoqualityreport")
+#Testing the Subprocess module for sending commands to the terminal for connecting to NordVPN proxy servers
+def testTerminalCommands():
+    command = "nordvpn -c -g new york"
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     
-    time.sleep(1)
-
-    try:
-        change_location_element = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='results-title ellipsize']/span[3]"))
-        )    
-    except:
-        print("{} Failed. Change location element not found.".format(state))
-        driver.quit()
-        return
-    
-    mouse.move_to_element(change_location_element)
-    mouse.click()
-    mouse.perform()
-
-    try:
-        location_input_element = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='modal-dialog-content']/div[1]/input[1]"))
-        )
-
-        ok_button = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, "//div[@class='modal-dialog-buttons']/button[1]"))
-        )
-    except:
-        print("{} Failed. Location input element or ok button element not found.".format(state))
-        driver.quit()
-        return
-    
-    try:
-        location_input_element.send_keys(state)
-    except:
-        print("{} Failed. Unable to pass state name into input element.".format(state))
-        driver.quit()
-        return
-    
-    #Find table elements that contain names of available locations from the search
-    correct_table_entry = None
-    try:
-        table_entrys = WebDriverWait(driver, 5).until(
-            EC.presence_of_all_elements_located((By.CLASS_NAME, 'autocomplete-row'))
-        )
-        for entry in table_entrys:
-            if entry.text == state:
-                correct_table_entry = entry
-
-    except:
-        print("{} Failed. State not found in list of locations. Check spelling.".format(state))
-        driver.quit()
-        return
-
-    time.sleep(1)
-
-    try:
-        mouse.move_to_element(correct_table_entry)
-        mouse.click()
-        mouse.perform()
-
-        time.sleep(0.25)
-
-        mouse.move_to_element(ok_button)
-        mouse.click()
-        mouse.perform()
-    except:
-        print("{} Failed. Error redirecting to new location.".format(state))
-        driver.quit()
-        return
-
-    time.sleep(1)
-
-    driver.quit()
+    output, error = process.communicate()
+    print(output.decode("ascii"))
+    print(error.decode("ascii"))
 
 #Main Method
 if __name__ == "__main__":
     # testToggleButton("https://www.google.com/get/videoqualityreport/")
-    #testCropImage
-    # print(testRegularExpression("89% 12PM"))
-    # print(testRegularExpression("0%"))
-    # print(testRegularExpression("100"))
-    # print(testRegularExpression("--"))
-    # print(testRegularExpression("seventy two"))
-
+    #testCropIma
+    print(testRegularExpression("89% 12PM"))
+    print(testRegularExpression("0%"))
+    print(testRegularExpression("100"))
+    print(testRegularExpression("--"))
+    print(testRegularExpression("seventy two"))
     
-    testAllStates("Delaware")
-
     
