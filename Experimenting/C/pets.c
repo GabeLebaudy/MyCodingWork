b92baeaf92a8dca34c8a3b228289e26c2e9957bf
gl523 <gabe.lebaudy6@gmail.com>
2024-05-25 20:28:51 -0400

    Centralizing Code

---
+++ Experimenting/C/pets.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+struct Pet{
+    char name[16]; // max 15 characters (plus room for null terminator)
+    int age;
+    char species[16];
+};
+
+struct Pet* resize(struct Pet *arr, int n) {
+    arr = (struct Pet*) realloc(arr, n * sizeof(struct Pet));
+    return arr;
+}
+
+int main() {
+    //Initialize Vars
+    FILE *input = stdin;
+
+    char *s = NULL;
+    size_t nbytes = 0;
+    ssize_t nchars;
+    int count = 0;
+    int size = 1;
+    struct Pet *pets = malloc(size * sizeof(struct Pet));
+
+    // Read lines from the input
+    while ((nchars = getline(&s, &nbytes, input)) != -1) {
+        if (s == NULL) exit(1);
+
+        //clear newline
+        s[nchars-1]='\0';
+        nchars--;
+
+        //Resize pet array if necessary
+        if (size <= count) {
+            size *= 2;
+            resize(pets, size);
+        }
+
+        if (count % 3 == 0) {
+            strncpy(pets[count / 3].name, s, 15);
+            pets[count / 3].name[15] = '\0';
+        }
+
+        if (count % 3 == 1) {
+            pets[count / 3].age = atoi(s);
+        }
+
+        if (count % 3 == 2) {
+            strncpy(pets[count / 3].species, s, 15);
+            pets[count / 3].species[15] = '\0';
+        }
+        count++;
+    }
+    free(s);
+    fclose(input);
+
+    if (count % 3 != 0) {
+        printf("Error: The final pet is missing information.");
+        return 1;
+    }
+
+    //Print out pet data
+    for (int i = 0; i < count / 3; i++) {
+        printf("Pet %d: Name=%s, Age=%d, Species=%s\n", i + 1, pets[i].name, pets[i].age, pets[i].species);
+    }
+
+    return 0;
+}
