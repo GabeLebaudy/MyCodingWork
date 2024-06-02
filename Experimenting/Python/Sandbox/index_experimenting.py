cfcb9a7cf41d97822f035ed2cc281b811d07b437
GabeLebaudy <99764213+GabeLebaudy@users.noreply.github.com>
2024-06-01 21:28:11 -0400

    Central

--- Experimenting/Python/Sandbox/index_experimenting.py
+++ Experimenting/Python/Sandbox/index_experimenting.py
@@ -1,1 +1,24 @@
-print("test {}".format(str(5) + "AM"))
+def caesarCipher(s, k):
+    new_str = ""
+    for char in s:
+        new_char = char
+        ord_num = ord(char)
+        if 65 <= ord_num <= 90:
+            ord_num += k
+            if ord_num > 90:
+                ord_num -= 26
+                
+            new_char = chr(ord_num)
+            
+        if 97 <= ord_num <= 122:
+            ord_num += k
+            if ord_num > 122:
+                ord_num -= 26
+            
+            new_char = chr(ord_num)
+            
+        new_str += new_char
+    return new_str
+
+
+print(caesarCipher("www.abc.xy", 87))
